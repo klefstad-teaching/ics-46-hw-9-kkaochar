1b7f99a688c352917703b2c31f4a2a583a80d72b
Kailee Kaocharoen <kkaochar@uci.edu>
2025-03-10 18:27:04 +0000

    dijkstra gtest added

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -3,6 +3,12 @@
 #include "dijkstras.h"
 #include "ladder.h"
 
-TEST(Replace, Me) {
-  EXPECT_TRUE(false);
+TEST(Dijkstras, test1) {
+  Graph g;
+  file_to_graph("./src/small.txt", g);
+  vector<int> prev(g.numVertices);
+  vector<int> distances = dijkstra_shortest_path(g, 0, prev);
+  vector<int> shortest_path = extract_shortest_path(distances, prev, 0);
+  ASSERT_EQ(0, distances[0]);
+  ASSERT_EQ(0, shortest_path[0]);
 }
