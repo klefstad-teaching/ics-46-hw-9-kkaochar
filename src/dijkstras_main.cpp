cd6d63c3c3bb6d94d323ce57bb2290c7b2b6f02c
Kailee Kaocharoen <kkaochar@uci.edu>
2025-03-10 17:52:17 +0000

    dijkstras_main default opens the small.txt file and calls on the algorithm for each node

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -1,6 +1,12 @@
 #include "dijkstras.h"
 
 int main(int argc, char *argv[]) {
-    if (argc > 1) cout << argv << endl;
-    return 0;
+    string input_file = argc > 1 ? argv[1] : "small.txt";
+    Graph g;
+    file_to_graph(input_file, g);
+    vector<int> previous(g.numVertices);
+    vector<int> distances = dijkstra_shortest_path(g, 0, previous);
+    for (int i = 0; i < g.numVertices; ++i) {
+        print_path(extract_shortest_path(distances, previous, i), distances[i]);
+    }
 }
