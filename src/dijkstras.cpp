3057084a831ae8164a9f4cf9dbd722472d135c54
Kailee Kaocharoen <kkaochar@uci.edu>
2025-03-10 17:51:32 +0000

    dijkstras implemented and tested; MinComp used to make custom minimum heap; extract_shortest path uses a list to push to the front in constant time and then uses a constructor to transform it into a vector. print path insets a newline

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,22 +1,50 @@
 #include "dijkstras.h"
+#include <list>
+
+struct MinComp {
+    bool operator() (pair<int,int> a, pair<int,int> b) {
+        return a.second < b.second;
+    }
+};
 
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
-    cout << G << source << previous << endl;
-    vector<int> shortest_path;
-    return shortest_path;
+    vector<int> distances(G.numVertices, INF);
+    vector<bool> visited(G.numVertices, false);
+    distances[source] = 0;
+    previous[source] = -1; // undefined
+    
+    priority_queue<pair<int,int>, vector<pair<int,int>>, MinComp> minHeap; // pair<vertex,weight>
+    minHeap.push({source,0});
+    
+    while(!minHeap.empty()) {
+        int u = minHeap.top().first; // gets vertex from the pair
+        minHeap.pop();
+        
+        if (visited[u]) continue;
+        visited[u] = true;
+        for (Edge e : G[u]) {
+            int v = e.dst;
+            int weight = e.weight;
+            if (!visited[v] && distances[u] + weight < distances[v]) {
+                distances[v] = distances[u] + weight;
+                previous[v] = u;
+                minHeap.push({v,distances[v]});
+            }
+        }
+    }
+    return distances;
 }
 
 vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination) {
-    vector<int> shortest_path;
-    for(int e = previous[destination]; e != -1 ; e = previous[e]) {
-        shortest_path.push_back(e);
-    }
-    shortest_path.push_back(0);
+    list<int> list_path;
+    for(int e = destination; e != -1; e = previous[e])
+        list_path.push_front(e);
+    vector<int> shortest_path(list_path.begin(), list_path.end());
     return shortest_path;
 }
 
 void print_path(const vector<int>& v, int total) {
     for (int e : v)
         cout << e << ' ';
-    cout << "Total cost is " << total << endl;
+    cout << "\nTotal cost is " << total << endl;
 }
