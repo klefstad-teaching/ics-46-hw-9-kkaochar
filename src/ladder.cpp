8f6f9fc00d4addc96342ca2b8790b948a643f89b
Kailee Kaocharoen <kkaochar@uci.edu>
2025-03-11 13:53:06 +0000

    formatting of print updated

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -65,9 +65,12 @@ void load_words(set<string> & word_list, const string& file_name) {
 }
 
 void print_word_ladder(const vector<string>& ladder) {
-    if (ladder.size() == 0) cout << "No word ladder found.";
-    for (string word : ladder)
-        cout << word << ' ';
+    if (ladder.size() == 0) {
+        cout << "No word ladder found.";
+    } else {
+        cout << "Word ladder found: ";
+        for (string word : ladder)
+            cout << word << ' ';
     cout << '\n';
 }
 
