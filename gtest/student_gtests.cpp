37863d6b696407e99feb7b4cf486cf299600ea37
Kailee Kaocharoen <kkaochar@uci.edu>
2025-03-11 13:35:36 +0000

    gtests added to check genereate word ladder works

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -12,3 +12,39 @@ TEST(Dijkstras, test1) {
   ASSERT_EQ(0, distances[0]);
   ASSERT_EQ(0, shortest_path[0]);
 }
+
+TEST(Ladder, test1) {
+  set<string> word_list;
+  load_words(word_list, "./src/words.txt");
+  ASSERT_EQ(generate_word_ladder("cat", "dog", word_list).size(), 4);
+}
+
+TEST(Ladder, test2) {
+  set<string> word_list;
+  load_words(word_list, "./src/words.txt");
+  ASSERT_EQ(generate_word_ladder("marty", "curls", word_list).size(), 6);
+}
+
+TEST(Ladder, test3) {
+  set<string> word_list;
+  load_words(word_list, "./src/words.txt");
+  ASSERT_EQ(generate_word_ladder("code", "data", word_list).size(), 6);
+}
+
+TEST(Ladder, test4) {
+  set<string> word_list;
+  load_words(word_list, "./src/words.txt");
+  ASSERT_EQ(generate_word_ladder("work", "play", word_list).size(), 6);
+}
+
+TEST(Ladder, test5) {
+  set<string> word_list;
+  load_words(word_list, "./src/words.txt");
+  ASSERT_EQ(generate_word_ladder("sleep", "awake", word_list).size(), 8);
+}
+
+TEST(Ladder, test6) {
+  set<string> word_list;
+  load_words(word_list, "./src/words.txt");
+  ASSERT_EQ(generate_word_ladder("car", "cheat", word_list).size(), 4);
+}
